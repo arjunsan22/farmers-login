e0d1d8a3de0a82bfab87511209380699515a2411
arjunsan22 <arjunanil2114@gmail.com>
2025-05-13 19:38:42 +0530

    google console passport file some issue in callbackurL

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(new GoogleStrategy({
 
    clientID: process.env.GOOGLE_CLIENT_ID,
    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL: 'https://farmerslogin.site/auth/google/callback',
+   callbackURL:process.env.GOOGLE_CALLBACK_URL,
    prompt: 'select_account', 
 },
 async (accessToken,refreshToken,profile,done,req,res) => {
